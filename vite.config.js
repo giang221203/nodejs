d48cbd7018bac7362998677f91638a0cfd1865f6
Your Name <you@example.com>
2023-03-25 16:38:57 +0700

    m

---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vite';
+import { VitePluginNode } from 'vite-plugin-node';
+import dotenv from "dotenv";
+dotenv.config();
+const {PORT } = process.env;
+export default defineConfig({
+  // ...vite configures
+  server: {
+    // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+    port: PORT,
+  },
+  plugins: [
+    ...VitePluginNode({
+   
+      adapter: 'express',
+
+      appPath: './src/app.js',
+      exportName: 'viteNodeApp',
+
+      tsCompiler: 'esbuild',
+
+      // Optional, default: {
+      // jsc: {
+      //   target: 'es2019',
+      //   parser: {
+      //     syntax: 'typescript',
+      //     decorators: true
+      //   },
+      //  transform: {
+      //     legacyDecorator: true,
+      //     decoratorMetadata: true
+      //   }
+      // }
+      // }
+      // swc configs, see [swc doc](https://swc.rs/docs/configuration/swcrc)
+      swcOptions: {}
+    })
+  ],
+  optimizeDeps: {
+    // Vite does not work well with optionnal dependencies,
+    // you can mark them as ignored for now
+    // eg: for nestjs, exlude these optional dependencies:
+    // exclude: [
+    //   '@nestjs/microservices',
+    //   '@nestjs/websockets',
+    //   'cache-manager',
+    //   'class-transformer',
+    //   'class-validator',
+    //   'fastify-swagger',
+    // ],
+  },
+});
